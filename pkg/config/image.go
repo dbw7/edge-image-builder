a91e403b3eb9f741a9178af7daeace633d637339
dbw7 <90856056+dbw7@users.noreply.github.com>
2023-11-14 10:40:54 -0500

    temp initial changes for elemental

--- pkg/config/image.go
+++ pkg/config/image.go
@@ -12,14 +12,25 @@ const (
 )
 
 type ImageConfig struct {
-	APIVersion string `yaml:"apiVersion"`
-	Image      Image  `yaml:"image"`
+	APIVersion string    `yaml:"apiVersion"`
+	Image      Image     `yaml:"image"`
+	Elemental  Elemental `yaml:"elemental"`
 }
 
 type Image struct {
 	ImageType       string `yaml:"imageType"`
 	BaseImage       string `yaml:"baseImage"`
 	OutputImageName string `yaml:"outputImageName"`
+}
+
+type Elemental struct {
+	Registration struct {
+		RegistrationURL string `yaml:"url"`
+		CACert          string `yaml:"ca-cert"`
+		EmulateTPM      string `yaml:"emulate-tpm"`
+		EmulateTPMSeed  string `yaml:"emulated-tpm-seed"`
+		AuthType        string `yaml:"auth"`
+	} `yaml:"registration"`
 }
 
 func Parse(data []byte) (*ImageConfig, error) {
